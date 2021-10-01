3c26bb668afc0946455ec40ff30de951bf524843
Divyanshu-Modi <divyan.m05@gmail.com>
2021-10-01 11:03:07 +0530

    selinux: Remove audit dependency
    Squash of:
     selinux: don't require auditing (commit1)
     selinux: Allow audit to be disabled (commit2)
    
    commit 1:
      Audit is bad. It affects performance under all circumstances. It's also
      dirty.
    
      From LWN: "Andy submitted a patch to fix this particular problem, but he
      didn't stop there. He has come to the conclusion that the audit
      subsystem is beyond repair, so his patch marks the whole thing as being
      broken, making it generally inaccessible. He cited a number of problems
      beyond this security issue: it hurts performance even when it is not
      being used, it is not (in his mind) reliable, it has problems with
      various architectures, and "its approach to freeing memory is
      terrifying." All told, Andy said, we're better off without it"
    
    commit 2:
      SELinux doesn't actually need audit support. In fact, auditing is not
      particularly useful on production kernels, where minimal debugging is
      needed.
    
    Co-authored-by: kdrag0n <dragon@khronodragon.com>
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3567,6 +3567,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3589,6 +3590,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
