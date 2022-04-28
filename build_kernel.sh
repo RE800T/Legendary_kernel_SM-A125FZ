5035d5d8b80544e89d11bbac4fc819051521374e
RE800T <brutalman122@gmail.com>
2022-04-28 11:18:54 +0000

    build: Update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,64 @@
-#!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
-export ANDROID_MAJOR_VERSION=r
+#Set time zone to Moscow
+sudo ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+# Colors
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a12_00_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+echo -e "${green}"
+echo "–––––––––––––––––––––"
+echo "Cloning dependencies:"
+echo "–––––––––––––––––––––"
+echo -e "${restore}"
+git clone https://github.com/kdrag0n/proton-clang --depth=1 ~/toolchain/proton-clang
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 --depth=1 ~/toolchain/gcc
+git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9  --depth=1 ~/toolchain/gcc32
+echo -e "${green}"
+echo "––––"
+echo "Done"
+echo "––––"
+echo -e "${restore}"
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz
+START=$(date +"%s")
+KERNEL_DIR=$(pwd)
+PATH="${HOME}/toolchain/proton-clang/bin:${HOME}/toolchain/gcc/bin:${HOME}/toolchain/gcc32/bin:${PATH}"
+VERSION="$(cat arch/arm64/configs/a12_00_defconfig | grep "CONFIG_LOCALVERSION\=" | sed -r 's/.*"(.+)".*/\1/' | sed 's/^.//')"
+export KBUILD_BUILD_HOST=Revers
+export KBUILD_BUILD_USER="Sezam4ik"
+
+# Compile plox
+function compile() {
+    make O=out ARCH=arm64 a12_00_defconfig
+    make -j$(nproc --all) O=out \
+                    ARCH=arm64 \
+                    CC=clang \
+                    CLANG_TRIPLE=aarch64-linux-gnu- \
+                    CROSS_COMPILE=aarch64-linux-android- \
+                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+    if ! [ -a "$IMAGE" ]; then
+        exit 1
+    fi
+    cp out/arch/arm64/boot/Image.gz AnyKernel3
+}
+# Zipping
+function zipping() {
+    cd AnyKernel3 || exit 1
+    rm -rf *.zip
+    zip -r9 $VERSION-SM-A125FZ-$(date +%Y%m%d-%H%M).zip *
+    cd ..
+}
+
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+echo -e "${green}"
+echo "----------------------------------------------"
+echo "Build Completed in: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo "----------------------------------------------"
+echo -e "${restore}"
